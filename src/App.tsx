e21d259878fa57789f3c8e10c1d050e7fd842c9b
Rahul Singh <3rahuljs@gmail.com>
2024-07-17 23:09:48 +0530

    remove pagination from view

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import ArticleList from "./components/ArticleList";
 import Filters from "./components/Filters";
-import Pagination from "./components/Pagination";
 
 export default function App() {
   return (
     <div className="main-grid-container">
       <Filters />
       <ArticleList />
-      <Pagination />
     </div>
   );
 }
